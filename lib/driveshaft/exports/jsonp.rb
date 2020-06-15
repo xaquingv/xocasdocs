2c5a85af399da835bb4c363a524520ca2c182c53
Xaquin Veira <xocasgv@gmail.com>
2020-06-15 20:27:38 +0200

    Restarting

--- lib/driveshaft/exports/jsonp.rb
+++ lib/driveshaft/exports/jsonp.rb
@@ -4,8 +4,8 @@ require 'google/apis/sheets_v4'
 module Driveshaft
   module Exports
 
-    def self.spreadsheet(file, drive_service)
-      data = {}
+    def self.jsonp(file, drive_service)
+      functions = []
 
       sheets_service = Google::Apis::SheetsV4::SheetsService.new
       sheets_service.authorization = drive_service.authorization
@@ -15,24 +15,23 @@ module Driveshaft
         sheet_name = sheet.properties.title
         next if sheet_name.match(/:hide$/)
 
-        # There is currently no way to export data from the non-first sheet using the V3 Drive API.
-        # https://issuetracker.google.com/issues/36760272
-        # csv_data = drive_service.export_file(file.id, 'text/csv')
         csv_data = drive_service.http(:get, "https://docs.google.com/spreadsheets/export?id=#{file.id}&exportFormat=csv&gid=#{sheet.properties.sheet_id}").force_encoding(Encoding::UTF_8)
 
-        data[sheet_name] = []
+        entries = []
 
         table = CSV.parse(csv_data, headers: true)
         table.each do |row|
           entry = row.to_h
           entry.keys.select { |k| k && k.match(/:hide$/) }.each { |k| entry.delete(k) }
-          data[sheet_name] << entry
+          entries << entry
         end
+
+        functions << "#{sheet_name}(#{JSON.dump(entries)});"
       end
 
       return {
-        body: JSON.dump(data),
-        content_type: 'application/json; charset=utf-8'
+        body: functions.join("\n"),
+        content_type: 'application/javascript; charset=utf-8'
       }
     end
 
